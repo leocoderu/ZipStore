f4eb7bb67811f30f7da7df2a0209447974e76e3a
LeoCodeRu <leo@leocode.ru>
2020-06-19 22:19:09 +0300

    Adding Bootstrap. Adding Local Database .mdb Adding EF.

---
+++ ZipStore/ZipStore/Concrete/EFDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ZipStore.Entities;
+
+namespace ZipStore.Concrete
+{
+    public class EFDBContext : DbContext
+    {
+        public DbSet<ZipItem> ZipItems { get; set; }
+    }
+}
--- ZipStore/ZipStore/Controllers/HomeController.cs
+++ ZipStore/ZipStore/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ZipStore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Projects\ASP.NET\ZipStore\ZipStore\ZipStore\App_Data\zipstore_db.mdf;Integrated Security=True";
+
         public ActionResult Index()
         {
-            return View();
+            DataTable ZipList = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(conStr))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlData = new SqlDataAdapter(
+                    "SELECT s.Id, s.Vendor, s.Number, s.SearchVendor, s.SearchNumber, s.Description, s.Price, s.Count FROM zipItem s", sqlCon);
+                sqlData.Fill(ZipList);
+                sqlCon.Close();
+            }
+            return View(ZipList);
         }
 
         public ActionResult About()
